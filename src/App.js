f19e83b71dac86b55ef03b285113be03bcc58be6
Matt Goldhammer <Matt.Goldhammer@gmail.com>
2022-11-13 15:11:15 -0700

    added filler content into components to ensure they will render

--- src/App.js
+++ src/App.js
@@ -1,40 +1,6 @@
-import React, { useState } from "react";
-import "./App.css";
-import Header from "./components/Header";
-import About from "./components/About";
-import Portfolio from "./components/Portfolio";
-import Contact from "./components/Contact";
-import Resume from "./components/Resume" 
-import Footer from "./components/Footer";
+import React from "react";
+import Container from "./components/Container";
 
-function App() {
-
-  const [currentPage, setCurrentPage] = useState("About");
-
-  const renderPage = () => {
-    if (currentPage === "About") {
-      return <About />;
-    }
-    if (currentPage === "Portfolio") {
-      return <Portfolio />;
-    }
-    if (currentPage === "Contact") {
-      return <Contact />;
-    }
-    if (currentPage === "Resume") {
-      return <Resume />;
-    }
-  };
-
-  const handlePageChange = (page) => setCurrentPage(page);
-
-  return (
-    <div className="Portfolio-app">
-      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-      {renderPage()}
-      <Footer />
-    </div>
-  );
-}
+const App = () => <Container />;
 
 export default App;
